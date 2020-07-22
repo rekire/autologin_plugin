51d84ca67dfdb25f41749a40366bce23748eddbf
rekire <git@reki.re>
2020-07-22 23:02:18 +0200

    Invoke SecRequestSharedWebCredential to fetch login data

--- ios/Classes/SwiftAutologinPlugin.swift
+++ ios/Classes/SwiftAutologinPlugin.swift
@@ -14,10 +14,36 @@ public class SwiftAutologinPlugin: NSObject, FlutterPlugin {
       result(false)
       break
     case "getLoginData":
-        result(["hallo", "welt"])
+      checkSafariCredentialsWithCompletion(completion: {username, password in
+        result([username, password])
+      })
       break
     default:
       result(FlutterMethodNotImplemented)
     }
+  }
+    
+  private func checkSafariCredentialsWithCompletion(completion: @escaping ((_ username: String?, _ password: String?) -> Void)) {
+    SecRequestSharedWebCredential(.none, .none, {
+        (credentials: CFArray!, error: CFError?) -> Void in
+
+      if let error = error {
+        print("error: \(error)")
+        completion(nil, nil)
+      } else if CFArrayGetCount(credentials) > 0 {
+        let unsafeCred = CFArrayGetValueAtIndex(credentials, 0)
+        let credential: CFDictionary = unsafeBitCast(unsafeCred, to: CFDictionary.self)
+        let dict: Dictionary<String, String> = credential as! Dictionary<String, String>
+        let username = dict[kSecAttrAccount as String]
+        let password = dict[kSecSharedPassword as String]
+        DispatchQueue.main.async {
+          completion(username, password)
+        }
+      } else {
+        DispatchQueue.main.async {
+          completion(nil, nil)
+        }
+      }
+    });
   }
 }
