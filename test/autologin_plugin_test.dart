597badb5bb5228018800cae7b7a8678484b5bee5
rekire <git@reki.re>
2023-08-27 12:53:33 +0200

    Remove useless imports

--- test/autologin_plugin_test.dart
+++ test/autologin_plugin_test.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:autologin_plugin/autologin_plugin.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('autologin_plugin');
+  //const MethodChannel channel = MethodChannel('autologin_plugin');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
