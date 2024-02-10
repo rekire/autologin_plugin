db1fc0a540c1bc364dae79e48885a31b802e6226
rekire <git@reki.re>
2024-02-10 23:41:48 +0100

    Add a darwin test

---
+++ autologin_darwin/test/autologin_darwin_test.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:autologin_darwin/autologin_darwin.dart';
+import 'package:autologin_platform_interface/autologin_platform_interface.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('AutologinDarwin', () {
+    late AutologinDarwin autologin;
+    late List<MethodCall> log;
+    const expectedCompatibilities = Compatibilities(
+      isPlatformSupported: true,
+      canSafeSecrets: true,
+      canEncryptSecrets: true,
+    );
+    const expectedCredentials = Credential(username: 'foo', password: 'bar');
+
+    setUp(() async {
+      autologin = AutologinDarwin();
+
+      log = <MethodCall>[];
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(autologin.methodChannel, (methodCall) async {
+        log.add(methodCall);
+        switch (methodCall.method) {
+          case 'performCompatibilityChecks':
+            return jsonEncode(expectedCompatibilities.toJson());
+          case 'requestCredentials':
+            return jsonEncode(expectedCredentials.toJson());
+          case 'saveCredentials':
+            return 'true';
+          default:
+            return null;
+        }
+      });
+    });
+
+    test('can be registered', () {
+      AutologinDarwin.registerWith();
+      expect(AutologinPlatform.instance, isA<AutologinDarwin>());
+    });
+
+    test('performCompatibilityChecks returns expected value', () async {
+      final report = await autologin.performCompatibilityChecks();
+      //expect(
+      //  log,
+      //  <Matcher>[isMethodCall('performCompatibilityChecks', arguments: null)],
+      //);
+      expect(report, equals(expectedCompatibilities));
+    });
+
+    test('requestCredentials returns expected value', () async {
+      final credentials = await autologin.requestCredentials();
+      expect(
+        log,
+        <Matcher>[isMethodCall('requestCredentials', arguments: null)],
+      );
+      expect(credentials, equals(expectedCredentials));
+    });
+
+    // ignore_for_file: avoid_catching_errors
+    test('saveCredentials is not implemented', () async {
+      try {
+        final report = await autologin.saveCredentials(expectedCredentials);
+        fail('Expected UnimplementedError');
+      } on UnimplementedError catch (_) {
+        // expected for now
+      }
+      //expect(
+      //  log,
+      //  <Matcher>[isMethodCall('saveCredentials', arguments: expectedCredentials.toJson())],
+      //);
+      //expect(report, equals(true));
+    });
+  });
+}
