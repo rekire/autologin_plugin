ba5fc81b76da3cdedf6a18cf63b733b31175bea6
rekire <git@reki.re>
2024-04-01 20:38:43 +0200

    Use a map instead of JSON

--- autologin_darwin/darwin/Classes/AutologinPlugin.swift
+++ autologin_darwin/darwin/Classes/AutologinPlugin.swift
@@ -17,7 +17,7 @@ public class AutologinPlugin: NSObject, FlutterPlugin {
         let messenger = registrar.messenger
 #endif
         let channel = FlutterMethodChannel(
-            name: "autologin_darwin",
+            name: "autologin_plugin",
             binaryMessenger: messenger)
         let instance = AutologinPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
@@ -37,11 +37,7 @@ public class AutologinPlugin: NSObject, FlutterPlugin {
                         for credential in credentials {
                             if let account = credential[kSecAttrAccount as String] as? String,
                                let password = credential[kSecSharedPassword as String] as? String {
-                                let jsonCredentials = ["username": account, "password": password]
-                                if let jsonData = try? JSONSerialization.data(withJSONObject: jsonCredentials, options: []),
-                                   let jsonString = String(data: jsonData, encoding: .utf8) {
-                                    result(jsonString)
-                                }
+                                result(["username": account, "password": password])
                             }
                         }
                     }
@@ -50,33 +46,22 @@ public class AutologinPlugin: NSObject, FlutterPlugin {
                 result(FlutterError(code: "INVALID_ARGUMENT", message: "Argument must be a string", details: nil))
             }
         case "saveCredentials":
-            if let args = call.arguments as? String {
-                if let jsonData = args.data(using: .utf8) {
-                    do {
-                        if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
-                            let username = json["username"] as! CFString
-                            let password = json["password"] as! CFString
-                            let domain = json["domain"] as! CFString
-                            SecAddSharedWebCredential(domain, username, password) { (error) in
-                                if let error = error {
-                                    result(FlutterError(code: "FAILED",
-                                                        message: "Could not save credentials: \(error)",
-                                                        details: nil))
-                                } else {
-                                    result(true)
-                                }
-                            }
-                        } else {
-                            result(FlutterError(code: "INVALID_ARGUMENT", message: "Unable to parse arguments as JSON", details: nil))
-                        }
-                    } catch {
-                        result(FlutterError(code: "INVALID_ARGUMENT", message: error.localizedDescription, details: nil))
-                    }
+            guard let arguments = call.arguments as? [String: Any],
+                  let username = arguments["username"] as? String,
+                  let password = arguments["password"] as? String,
+                  let domain = arguments["domain"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid arguments", details: nil))
+                return
+            }
+
+            SecAddSharedWebCredential(domain as CFString, username as CFString, password as CFString) { (error) in
+                if let error = error {
+                    result(FlutterError(code: "FAILED",
+                                        message: "Could not save credentials: \(error)",
+                                        details: nil))
                 } else {
-                    result(FlutterError(code: "INVALID_ARGUMENT", message: "Unable to convert JSON string to data", details: nil))
+                    result(true)
                 }
-            } else {
-                result(FlutterError(code: "INVALID_ARGUMENT", message: "Argument must be a string", details: nil))
             }
         case "requestLoginToken":
             let keyValueStore = NSUbiquitousKeyValueStore.default
