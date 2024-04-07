89feb47b38145e16dbc4d2790954fbcdd9bda3f5
rekire <git@reki.re>
2024-04-07 11:15:13 +0200

    More formatting

--- autologin_platform_interface/lib/autologin_platform_interface.dart
+++ autologin_platform_interface/lib/autologin_platform_interface.dart
@@ -7,13 +7,16 @@ export 'package:autologin_platform_interface/src/compatibilities.dart'
     show Compatibilities;
 export 'package:autologin_platform_interface/src/credential.dart'
     show Credential;
-export 'package:autologin_platform_interface/src/method_channel_autologin.dart' show MethodChannelAutologin;
+export 'package:autologin_platform_interface/src/method_channel_autologin.dart'
+    show MethodChannelAutologin;
 
 /// The interface that implementations of autologin must implement.
 ///
-/// Platform implementations should extend this class rather than implement it as `Autologin`.
-/// Extending this class (using `extends`) ensures that the subclass will get the default implementation, while platform
-/// implementations that `implements` this interface will be broken by newly added [AutologinPlatform] methods.
+/// Platform implementations should extend this class rather than implement it
+/// as `Autologin`. Extending this class (using `extends`) ensures that the
+/// subclass will get the default implementation, while platform implementations
+/// that `implements` this interface will be broken by newly added
+/// [AutologinPlatform] methods.
 abstract class AutologinPlatform extends PlatformInterface {
   /// Constructs a AutologinPlatform.
   AutologinPlatform() : super(token: _token);
@@ -35,13 +38,16 @@ abstract class AutologinPlatform extends PlatformInterface {
     _instance = instance;
   }
 
-  /// Return the [Compatibilities] which depends on the platform or the browser in case of web
+  /// Return the [Compatibilities] which depends on the platform or the browser
+  /// in case of web.
   Future<Compatibilities> performCompatibilityChecks();
 
-  // Depending on your target platforms you need to setup attentional data. For iOS and MacOS the [domain] is required
-  // to ask the operating system for credentials for that domain. For Linux is an [appId] an [appName] required to
-  // select the credentials of your app (the name is in fact just for the label in the password manager). If you don't
-  // target those platforms you can omit this call.
+  /// Depending on your target platforms you need to setup attentional data. For
+  /// iOS and MacOS the [domain] is required to ask the operating system for
+  /// credentials for that domain. For Linux is an [appId] an [appName] required
+  /// to select the credentials of your app (the name is in fact just for the
+  /// label in the password manager). If you don't target those platforms you
+  /// can omit this call.
   void setup({String? domain, String? appId, String? appName});
 
   /// Returns `true` when the current platform is supported.
@@ -53,19 +59,23 @@ abstract class AutologinPlatform extends PlatformInterface {
     return _isPlatformSupported!;
   }
 
-  /// Returns the saved [Credential] when set by [saveCredentials] or is `null` when not found.
+  /// Returns the saved [Credential] when set by [saveCredentials] or is `null`
+  /// when not found.
   Future<Credential?> requestCredentials();
 
   /// Returns `true` when the [Credential]s could be saved, otherwise `false`.
   Future<bool> saveCredentials(Credential credential);
 
-  /// Returns the saved token which was set by [saveLoginToken] or is `null` when not found. This can be a refresh token
-  /// or anything else which you can use to identify the user. You should validate that this token is still valid before
-  /// authenticating the user. Depending on your use case asking for a second factor would be a good idea.
+  /// Returns the saved token which was set by [saveLoginToken] or is `null`
+  /// when not found. This can be a refresh token or anything else which you can
+  /// use to identify the user. You should validate that this token is still
+  /// valid before authenticating the user. Depending on your use case asking
+  /// for a second factor would be a good idea.
   Future<String?> requestLoginToken();
 
-  /// Returns `true` when the [token] could be saved, otherwise `false`. This token is intended to be synchronized by
-  /// the platform you are using. This can be a refresh token or anything else which you can use to identify the user.
-  /// For security reasons you should not store credentials.
+  /// Returns `true` when the [token] could be saved, otherwise `false`. This
+  /// token is intended to be synchronized by the platform you are using. This
+  /// can be a refresh token or anything else which you can use to identify the
+  /// user. For security reasons you should not store credentials.
   Future<bool> saveLoginToken(String token);
 }
