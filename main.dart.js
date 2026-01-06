9b5da61a223cc8cfbb5a7b1de784203b6a7108ba
rekire <git@reki.re>
2026-01-06 16:07:31 +0000

    Deploying to gh-pages from @ rekire/autologin_plugin@22c2a5713c7c0963eb6cdc32736d602f77e49b8b 🚀

--- main.dart.js
+++ main.dart.js
@@ -24154,7 +24154,7 @@ $S:2}
 A.ajj.prototype={
 $1(a){var s=A.cG().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a5cb96369ef86c7e85abf5d662a1ca5d89775053/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/1527ae0ec577a4ef50e65f6fefcfc1326707d9bf/":s)+a},
 $S:185}
 A.Fb.prototype={
 Qt(){var s=this.a47(),r=$.bj.bn().ImageFilter.MakeColorFilter(s,null)
