f4c268be89366933f617701d6ffd182b4ad647c8
René Kilczan <git@reki.re>
2026-01-17 17:33:00 +0100

    Fix typo in package name

--- autologin_darwin/darwin/autologin_darwin/Package.swift
+++ autologin_darwin/darwin/autologin_darwin/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .macOS("11.0")
     ],
     products: [
-        .library(name: "plugin-name", targets: ["autologin-darwin"])
+        .library(name: "plugin-name", targets: ["autologin_darwin"])
     ],
     dependencies: [],
     targets: [
