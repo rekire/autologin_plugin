c9ac8cbdeaff0ba188094eef523235a745561443
rekire <git@reki.re>
2026-06-19 12:22:12 +0000

    Deploying to gh-pages from @ rekire/autologin_plugin@7b4cf0e224db088a2bd5c771c79a997563d7557d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a362","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4084405958" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1944688499" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- main.dart.js
+++ main.dart.js
@@ -24401,7 +24401,7 @@ $S:2}
 A.aiL.prototype={
 $1(a){var s=A.cA().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/4c525dac5ebe5971c5708ef73558ed8edcf4a362/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
 $S:135}
 A.EI.prototype={
 RX(){var s=this.a5X(),r=$.b0.be().ImageFilter.MakeColorFilter(s,null)
