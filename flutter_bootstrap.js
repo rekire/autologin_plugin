16af4e63bbdc0cc16865705360782a125e1b824d
rekire <git@reki.re>
2026-08-07 13:27:47 +0000

    Deploying to gh-pages from @ rekire/autologin_plugin@b95f7c701bea41a73b6f2090ee644e667757c4bd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "708736674" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1518734903" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
